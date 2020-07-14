f8c1b5c10752c2b810aaeaeab2f42a1005650166
kanr <myhisung@163.com>
2020-07-14 11:40:51 +0800

    change key size

--- comm.h
+++ comm.h
@@ -5,14 +5,14 @@ typedef struct cmd_t{
     short row;
     short col;
     char  flag;
-    char  size;
-    char  key[4];
+    char  size;   // effect length of key
+    char  key[6]; // 6 byte enough to save any key
 } cmd_t;
 
 #define    SET_KEY        0x01
 #define    SET_WINDOW     0x02
 
-#define    VERSION        "1.2.4"
+#define    VERSION        "1.2.5"
 #define    BUFSIZE        1024
 
 #endif
